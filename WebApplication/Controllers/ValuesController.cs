0f0ccd27d491d7f85b2cba37c5ff98bcf9892ba4
peted70 <peted70@hotmail.com>
2015-02-13 12:59:08 +0000

    added conflicting change

--- WebApplication/Controllers/ValuesController.cs
+++ WebApplication/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace WebApplication.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "making conflict 2";
         }
 
         // POST api/values
